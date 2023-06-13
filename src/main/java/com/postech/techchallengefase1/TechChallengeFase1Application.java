f2862dc7bcb154ccb0c017f828c74b197f696b7c
Mayara Lima <may.veloso.lima@gmail.com>
2023-06-12 23:06:20 -0300

    first commit

---
+++ src/main/java/com/postech/techchallengefase1/TechChallengeFase1Application.java
@@ -0,0 +1,13 @@
+package com.postech.techchallengefase1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeFase1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeFase1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/postech/techchallengefase1/TechChallengeFase1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postech.techchallengefase1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeFase1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
